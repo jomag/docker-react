c7db524e1d078a6a07d37c5860ce6cdb162abffc
Jonatan Magnusson <jonatan.magnusson@above.se>
2019-01-31 10:04:26 +0100

    New feature yay!

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Why hello there!
+            New features yay!
           </p>
           <a
             className="App-link"
