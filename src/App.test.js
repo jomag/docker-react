22849b130264ece014433bc8857cd2f24b13b97e
Jonatan Magnusson <jonatan.magnusson@above.se>
2019-01-31 09:15:16 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  // throw Error('bajs');
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
